e31121b9a1e74dae1300b1e72a4fc75b6bf9b685
RealPolux <antonmg91@hotmail.com>
2025-11-05 11:09:51 +0100

    Pinia solved

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
-import piniaPluginPersistedState from "pinia-Plugin-PersistedState";
+import piniapluginPersistedState from "pinia-plugin-persistedstate";
 
 const pinia = createPinia();
-pinia.use(piniaPluginPersistedState);
+pinia.use(piniapluginPersistedState);
 
 createApp(App).use(router).use(pinia).mount("#app");
